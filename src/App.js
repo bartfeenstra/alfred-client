9c7461a66d7ba6386072e903ae9d122353bf7315
Bart Feenstra <bart@mynameisbart.com>
2017-04-05 22:37:58 +0200

    Finalize routing.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { slide as Menu } from 'react-burger-menu';
-import { Link, IndexLink } from 'react-router';
+import { Link, Route, BrowserRouter } from 'react-router-dom';
 import Radium from 'radium';
-import { Route } from 'react-router-dom';
 import Lights from './Lights';
 import GraphQlUi from './GraphQlUi';
 
 let RadiumLink = Radium(Link);
-let RadiumIndexLink = Radium(IndexLink);
 
 class App extends Component {
   render() {
     return (
+    <BrowserRouter>
       <div className="App">
         <div className="App-header">
           <h2>What can I do for you?</h2>
+          <Menu>
+            <RadiumLink to="/">Home</RadiumLink>
+            <RadiumLink to="/graphql-ui">GraphQL</RadiumLink>
+          </Menu>
         </div>
         {this.props.children}
-        <Menu>
-            <RadiumIndexLink to="/">Home</RadiumIndexLink>
-            <RadiumLink to="/graphql-ui">GraphQL</RadiumLink>
-        </Menu>
-        <Route exact path='/' component={Lights} />
-        <Route path='/graphql-ui' component={GraphQlUi} />
+        <Route exact path="/" component={Lights} />
+        <Route path="/graphql-ui" component={GraphQlUi} />
       </div>
+    </BrowserRouter>
     );
   }
 }
-
-//App.propTypes = {
-//    children: PropTypes.object.isRequired
-//};
 
 export default App;
