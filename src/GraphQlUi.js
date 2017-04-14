f36247223d730df68a13f301696ca76b4756ece2
Bart Feenstra <bart@mynameisbart.com>
2017-04-14 13:09:31 +0200

    Implement changes in the GraphQL API.

--- src/GraphQlUi.js
+++ src/GraphQlUi.js
@@ -3,10 +3,10 @@ import GraphiQL from 'graphiql';
 import '../node_modules/graphiql/graphiql.css';
 
 function graphQLFetcher(graphQLParams) {
-  // @todo These headers somehow still result in a text/plain request being made.
   let headers = new Headers();
+  headers.append('Accept', 'application/json');
   headers.append('Content-Type', 'application/json');
-  return fetch('http://localhost:5000', {
+  return fetch('http://localhost:8000', {
     method: 'POST',
     headers: headers,
     body: JSON.stringify(graphQLParams),
