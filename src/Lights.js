46779712b2b8f2031e709e0a03e036a8ca486804
Bart Feenstra <bart@mynameisbart.com>
2017-04-24 18:42:59 +0200

    Initial (broken) commit.

--- src/Lights.js
+++ src/Lights.js
@@ -8,18 +8,30 @@ class Lights extends Page {
     constructor(props) {
         super(props);
         this.state = {
-          color: 'black',
+          color: '#000000',
         };
-    }
-    onDrag(color, c) {
-        this.setState({color});
-        this._setLights()
     }
 
     renderContent() {
+        let data = this.props.data;
+        // data.startPolling(100);
+        console.log(this.state);
+        console.log(this.props.data);
+        // const { data: {lights} } = this.props;
+        // console.log(this.props.data.error);
+        if (data.loading) {
+            return (
+                <p>Loading...</p>
+            );
+        }
+        if (data.error) {
+            return (
+                <p>{data.error.toString()}</p>
+            );
+        }
         return (
             <div className="Lights">
-                <ColorPicker value={this.state.color} onDrag={this.onDrag.bind(this)} />
+                <ColorPicker value={data.lights.color} onDrag={this._setLights.bind(this)} />
             </div>
         );
     }
@@ -28,18 +40,18 @@ class Lights extends Page {
         return 'Lights';
     }
 
-    _setLights(event) {
-        console.log(this.state);
+    _setLights(color) {
         this.props.mutate({
             variables: {
-                red: parseInt(this.state.color.substring(1, 3), 16),
-                green: parseInt(this.state.color.substring(3, 5), 16),
-                blue: parseInt(this.state.color.substring(5, 7), 16),
+                color: color,
                 luminosity: 255,
             }
         })
         .then(({ data }) => {
-            console.log('got data', data);
+            this.props.data.lights = {
+                color: data.color,
+            };
+            this.forceUpdate();
         })
         .catch((error) => {
             console.log('there was an error sending the query', error);
@@ -48,24 +60,34 @@ class Lights extends Page {
 }
 
 
+const getLightsQuery = gql`
+{
+    lights {
+        color,
+        luminosity
+    }
+}
+`;
+
+
 const setLightsQuery = gql`
-mutation ($red: Int!, $green: Int!, $blue: Int!, $luminosity: Int!) {
+mutation ($color: String!, $luminosity: Int!) {
     setLights(
         lights: {
-            red: $red,
-            green: $green,
-            blue: $blue,
+            color: $color,
             luminosity: $luminosity,
         }
     ) {
         lights {
-            red,
-            green,
-            blue,
+            color,
             luminosity
         }
     }
 }
 `;
 
-export default graphql(setLightsQuery)(Lights);
+export default graphql(getLightsQuery, {
+    options: {
+        pollInterval: 10000,
+    },
+})(graphql(setLightsQuery)(Lights));
